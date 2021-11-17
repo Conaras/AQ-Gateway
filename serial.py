609477b676ebf0e7176623ebc6f613549785af97
Conaras <50587129+Conaras@users.noreply.github.com>
2021-11-17 13:59:16 +0200

    Update serial.py

--- serial.py
+++ serial.py
@@ -12,7 +12,7 @@ timeout=1)
 i=0
 j=0
 c=0
-file.write("PM1.0 Standard\tPM2.5 Standard\tPM10.0 Standard\tPM1.0 Environmental\tPM2.5 Standard\tPM10.0 Environmental\tParticles > 0.3um / 0.1L air\tParticles > 0.5um / 0.1L air\tParticles > 1.0um / 0.1L air\tParticles > 2.5um / 0.1L air\tParticles > 5.0um / 0.1L air\tParticles > 10.0um / 0.1L air\n")
+file.write("PM1.0 Standard\tPM2.5 Standard\tPM10.0 Standard\tPM1.0 Environmental\tPM2.5 Environmental\tPM10.0 Environmental\tParticles > 0.3um / 0.1L air\tParticles > 0.5um / 0.1L air\tParticles > 1.0um / 0.1L air\tParticles > 2.5um / 0.1L air\tParticles > 5.0um / 0.1L air\tParticles > 10.0um / 0.1L air\n")
 print("connected to: " + ser.portstr)
 print()
 while True:
